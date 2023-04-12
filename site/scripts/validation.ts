de2c0ff3aad62fa3ed891791bc51d06a7567fe8e
Edvin Dzidic <edvindzidic2000@gmail.com>
2023-04-12 19:18:48 +0200

    Implement exit after error, remove todo, other changes

--- site/scripts/validation.ts
+++ site/scripts/validation.ts
@@ -2,12 +2,7 @@ import { parsePreamble } from "../lib/preamble";
 import { JipId, getAllJipIDs, getJIPDirectory, isStringJipId } from "../lib/files";
 import { FileArgument } from "./types";
 import { success, warning, info, failure } from "./util";
-import path from "path";
 import fs from "fs";
-
-// TODO:
-// 1. Idea: If there is an argument passed (file) then we validate that file.
-//    If not we validate all folders that have the format jip-${number}.
 
 const fileArgument = process.argv[2] as undefined | FileArgument;
 
@@ -22,6 +17,7 @@ const validateFile = (jipId: JipId) => {
     console.log(success("File successfuly validated, no problems found!"));
   } catch (e) {
     console.log(failure(`There has been a problem while trying to validate this file.\n${e}`));
+    throw e;
   }
 };
 
@@ -33,14 +29,24 @@ const validateFiles = () => {
 };
 
 const main = async () => {
-  if (fileArgument && isStringJipId(fileArgument)) {
-    validateFile(fileArgument as JipId);
-  } else {
-    console.log(
-      warning(`No file argument has been passed, testing all conforming files in the root folder!`)
-    );
-    validateFiles();
+  try {
+    if (fileArgument && isStringJipId(fileArgument)) {
+      validateFile(fileArgument as JipId);
+    } else {
+      console.log(
+        warning(
+          `No file argument has been passed, testing all conforming files in the root folder!`
+        )
+      );
+      validateFiles();
+    }
+  } catch (e) {
+    console.log("Error occured, exiting..");
+
+    return -1;
   }
+
+  return 0;
 };
 
 main();
